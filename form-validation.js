5ca1ff1a4a3632ef8888a4326c5e54529ef6d5e2
Unknown <aspruce@mail.csuchico.edu>
2017-03-09 17:48:36 -0800

    Errors

--- form-validation.js
+++ form-validation.js
@@ -2,18 +2,39 @@ function validate(){
   //Grab the user's input and store in variables
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
+  var length = userEntered.length;
 
-  //Show message that there is an error with the username...
-  document.getElementById("usernameError").innerHTML="Bad username.";
-  document.getElementById("usernameError").classList.remove("hidden-message");
-  document.getElementById("usernameError").classList.add("shown-message");
-  //Turn the username items red
-  document.getElementById("usernameGroup").classList.add("has-error");
 
-  //Show message that there is an error with the password...
-  document.getElementById("passwordError").innerHTML="Bad password.";
-  document.getElementById("passwordError").classList.remove("hidden-message");
-  document.getElementById("passwordError").classList.add("shown-message");
-  //Turn the password items red
-  document.getElementById("passwordGroup").classList.add("has-error");
+
+
+  if (length >= 6)
+  {
+    document.getElementById("usernameError").innerHTML="";
+    document.getElementById("usernameError").classList.remove("hidden-message");
+    document.getElementById("usernameError").classList.add("shown-message");
+    //Turn the username items green
+    document.getElementById("usernameGroup").classList.add("has-success");
+  }
+  else if (length <= 5)
+  {
+    document.getElementById("usernameError").innerHTML="Bad username.";
+    document.getElementById("usernameError").classList.remove("hidden-message");
+    document.getElementById("usernameError").classList.add("shown-message");
+    //Turn the username items red
+    document.getElementById("usernameGroup").classList.add("has-error");
+  }
+  if (passEntered == "password")
+  {
+    //Show message that there is an error with the password...
+    document.getElementById("passwordError").innerHTML="Bad password.";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    //Turn the password items red
+    document.getElementById("passwordGroup").classList.add("has-error");
+  }
+  else
+  {
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordGroup").classList.add("has-success");
+  }
 }
